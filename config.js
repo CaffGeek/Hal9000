a45c5df3055035a9e856229d2df9e9bc39cb0d5f
Chad Hurd <gibble+github@gmail.com>
2016-10-17 15:10:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
 config.bot_api_key = process.env.SLACK_TOKEN || 'slack_api_key';
-config.botname = process.env.BOT_NAME || 'Hal9000';
+config.botname = process.env.BOT_NAME || 'hal9000';
 
 module.exports = config;
