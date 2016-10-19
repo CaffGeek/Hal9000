ed2211c3754faa089502a6fbaf87601bb4801af9
caffgeek <gibble@gmail.com>
2016-10-18 21:56:26 -0500

    missing update to run.js

--- run.js
+++ run.js
@@ -7,6 +7,7 @@ console.log('config.bot_api_key=%s', config.bot_api_key);
 require('skellington')({
 	slackToken: config.bot_api_key,
 	plugins: [
-		require('./lib/hush')
+		require('./lib/hush/hush.js'),
+		require('./lib/mood/mood.js')
 	]
 });
