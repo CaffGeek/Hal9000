d62ef78b52ab8711a2f582e8411d43af6a945c94
caffgeek <gibble@gmail.com>
2016-10-18 21:48:05 -0500

    markov...removed

--- run.js
+++ run.js
@@ -7,7 +7,6 @@ console.log('config.bot_api_key=%s', config.bot_api_key);
 require('skellington')({
 	slackToken: config.bot_api_key,
 	plugins: [
-		require('./lib/hush'),
-		require('skellington-markov')
+		require('./lib/hush')
 	]
 });
