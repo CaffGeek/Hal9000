032540cdb871f15f06bc02cf63ef2a11aed02315
caffgeek <gibble@gmail.com>
2016-10-18 20:44:35 -0500

    changing to use skellington

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 var config = {};
 
 config.bot_api_key = process.env.SLACK_TOKEN || 'slack_api_key';
-config.botname = process.env.BOT_NAME || 'hal9000';
 
 module.exports = config;
--- run.js
+++ run.js
@@ -1,15 +1,10 @@
 'use strict';
 
 var config = require('./config.js');
-var Hal9000 = require('./lib/hal9000.js');
 
 console.log('config.bot_api_key=%s', config.bot_api_key);
-console.log('config.botname=%s', config.botname);
 
-var hal9000 = new Hal9000(
-{
-	token: config.bot_api_key,
-	name: config.botname
+require('skellington')({
+	slackToken: config.bot_api_key,
+	plugins: [require('./lib/hush')]
 });
-
-hal9000.run();
