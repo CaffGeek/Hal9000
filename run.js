9f9b4ba4694a70b725c141c7e155ae1b1fbbd894
caffgeek <gibble@gmail.com>
2016-10-19 21:57:33 -0500

    add storage file

--- run.js
+++ run.js
@@ -5,11 +5,12 @@ var config = require('./config.js');
 console.log('config.bot_api_key=%s', config.bot_api_key);
 
 require('skellington')({
+  	json_file_store: 'hal9000_storage.json',
 	slackToken: config.bot_api_key,
 	plugins: [
 		require('./lib/hush/hush.js'),
 		require('./lib/status/status.js'),
-		
+
 		require('./lib/remember/remember.js'),
 		require('./lib/remember/doyouremember.js'),
 		require('./lib/remember/whatdoyouremember.js')
