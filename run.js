f683d8f415d10959a3be73b07eaa7b1df24b5b2b
Chad Hurd <gibble@gmail.com>
2016-10-21 17:40:09 -0500

    storage...

--- run.js
+++ run.js
@@ -4,8 +4,7 @@ var config = require('./config.js');
 
 console.log('config.bot_api_key=%s', config.bot_api_key);
 
-require('skellington')({
-  	json_file_store: './storage',
+var controller = require('skellington')({
 	slackToken: config.bot_api_key,
 	plugins: [
 		require('./lib/hush/hush.js'),
@@ -16,3 +15,7 @@ require('skellington')({
 		require('./lib/remember/whatdoyouremember.js')
 	]
 });
+
+console.log(controller);
+console.log(controller.json_file_store);
+controller.json_file_store = './storage/';
