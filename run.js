8dc186f20e959f10f582b5fe62aa1618d221fb94
Chad Hurd <gibble@gmail.com>
2016-10-21 17:49:04 -0500

    mabe this

--- run.js
+++ run.js
@@ -4,7 +4,8 @@ var config = require('./config.js');
 
 console.log('config.bot_api_key=%s', config.bot_api_key);
 
-var controller = require('skellington')({
+require('skellington')({
+  	storage: simple_storage({path: './storage/'}),
 	slackToken: config.bot_api_key,
 	plugins: [
 		require('./lib/hush/hush.js'),
@@ -15,7 +16,3 @@ var controller = require('skellington')({
 		require('./lib/remember/whatdoyouremember.js')
 	]
 });
-
-console.log(controller);
-console.log(controller.json_file_store);
-controller.json_file_store = './storage/';
