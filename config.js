9376700089e07170a5345693160ab66231f74d97
caffgeek <gibble@gmail.com>
2017-01-17 22:29:37 -0600

    converting to slapp, probably totally broke everything

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var config = {};
 
-config.bot_api_key = process.env.SLACK_TOKEN || 'slack_api_key';
+config.verify_token = process.env.SLACK_VERIFY_TOKEN || 'verify_token';
+config.port = process.env.PORT || '3000';
 
 module.exports = config;
---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express')
+
+const Slapp = require('slapp')
+const ConvoStore = require('slapp-convo-beepboop')
+const Context = require('slapp-context-beepboop')
+
+var config = require('./config.js');
+var storage = require('./azure_storage.js');
+
+console.log('config.bot_api_key=%s', config.bot_api_key);
+
+var port = config.port;
+
+var slapp = Slapp({
+  // Beep Boop sets the SLACK_VERIFY_TOKEN env var
+  verify_token: config.verify_token,
+  convo_store: ConvoStore(),
+  context: Context()
+});
+
+
+var remember = require('./modules/remember/index.js');
+remember.init(slapp);
+
+var server = slapp.attachToExpress(express());
+server.listen(port, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log(`Listening on port ${port}`);
+});
