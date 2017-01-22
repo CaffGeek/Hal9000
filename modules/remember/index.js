a806760625d7ef9336031cdd8f68c55d92471112
caffgeek <gibble@gmail.com>
2017-01-22 16:59:13 -0600

    delay

--- modules/remember/index.js
+++ modules/remember/index.js
@@ -23,11 +23,15 @@ module.exports = {
       })
       .action('handleWho', (message, state) => {
 	      message.respond('debug in action handleWho');
+        setTimeout(function () { console.log('waiting...'); }, 3000);
+        
         trainer.handleWho(message,state);
         trainer.askHow(message, state); 
       })
       .action('handleHow', (message, state) => {
 	      message.respond('debug in action handleHow');
+        setTimeout(function () { console.log('waiting...'); }, 3000);
+
         trainer.handleHow(message, state);
         trainer.remember(message, state); 
       })
